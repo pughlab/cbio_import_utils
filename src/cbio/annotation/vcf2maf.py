c3aea078576b4b348c5e63593984d597124dd39f
Kelsey Zhu <kzhu@uhnresearch.ca>
2018-01-30 12:59:53 -0500

    add script to convert VEP vcf to maf file

---
+++ src/cbio/annotation/vcf2maf.py
@@ -0,0 +1,106 @@
+import os
+import argparse
+from string import Template
+
+def get_options():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--vep_data", type=str, required=False,
+                        help="reference genome cache data", default='/mnt/work1/users/pughlab/references/VEP_fasta/83_GRCh37')
+    parser.add_argument("--vep_path", type=str, required=False,
+                        help="path to VEP installation", default="/mnt/work1/software/vep/83")
+    parser.add_argument("-f", "--fasta", type=str, required=False,
+                        help="reference genome fasta file including path",
+                        default="/mnt/work1/users/pughlab/references/VEP_fasta/83_GRCh37/Homo_sapiens.GRCh37.75.dna.primary_assembly.fa")
+    parser.add_argument("-c", "--cwd", type=str, required=True,
+                        help="working directory")
+    parser.add_argument("-i", "--input", type=str, required=True,
+                        help="input vep file path")
+    parser.add_argument("-o", "--output", type=str, required=True,
+                        help="vep output directory")
+    parser.add_argument("--ncbi_build", type=str,
+                        help="NCBI genome build name e.g. GRCh37", default="GRCh37")
+    parser.add_argument("--vcf_TUMOR", type=str, required=False,
+                        help="vcf TUMOR sample column name", default="TUMOR")
+    parser.add_argument("--vcf_NORMAL", type=str, required=False,
+                        help="NCBI genome build name e.g. GRCh37", default="NORMAL")
+    parser.add_argument("--maf_center", type=str, required=False,
+                        help="variant tool center", default="mutect")
+    parser.add_argument("--filter_vcf", type=str, required=False,
+                        help="filter vcf file",
+                        default="/mnt/work1/users/pughlab/cBioportal_import/vep_plugin_dir/ExAC.r0.3.sites.minus_somatic.vcf.gz")
+    parser.add_argument("--separator", type=str, required=False,
+                        help="charactor used to separate sample ID", default="__")
+    parser.add_argument("-t", "--debug", action="store_true",
+                        help="debug mode for testing")
+    return parser.parse_args()
+
+def get_sample_IDs(sample_name, separator):
+    try:
+        return sample_name.strip().split(separator)
+    except:
+        return None, None
+
+def main():
+    args = get_options()
+    print(args)
+    tool_center = "mutec"
+    tmp_dir='/Users/kelsyzhu/workspace/data/importer/low_grade'
+    vep_dir='/Users/kelsyzhu/workspace/data/importer/low_grade/VEP'
+
+    s = Template('perl /mnt/work1/users/pughlab/cBioportal_import/import_wrapper/scripts/v1.6.12/vcf2maf-1.6.12/vcf2maf.pl \
+    --vep-data $vep_data \
+    --vep-path $vep_path \
+    --ref-fasta $fasta \
+    --tmp-dir $tmp_dir \
+    --ncbi-build $ncbi_build \
+    --vcf-tumor-id $vcf_TUMOR \
+    --vcf-normal-id  $vcf_NORMAL \
+    --tumor-id $sample_id \
+    --normal-id $normal_id \
+    --input-vcf $input_vcf \
+    --maf-center $maf_center \
+    --filter-vcf /mnt/work1/users/pughlab/cBioportal_import/vep_plugin_dir/ExAC.r0.3.sites.minus_somatic.vcf.gz \
+    --output-maf $maf')
+
+    for subdir, dirs, files in os.walk(vep_dir):
+        for file in files:
+            if file.endswith("vep.vcf"):
+                maf_file = os.path.join(args.output, "{}.maf".format(file.split(".")[0]))
+                if os.path.exists(maf_file): continue
+                vep_file = os.path.join(vep_dir, file)
+                print (vep_file)
+                sample_id, normal_id = get_sample_IDs(file.split(".")[0], args.separator)
+                if not sample_id: continue
+                print ("sample ID: {}".format(sample_id))
+                print ("matched normal ID: {}".format(normal_id))
+
+                d = dict(
+                        vep_data=args.vep_data,
+                        vep_path=args.vep_path,
+                        fasta=args.fasta,
+                        ncbi_build=args.ncbi_build,
+                        tmp_dir=args.cwd,
+                        vcf_TUMOR=args.vcf_TUMOR,
+                        vcf_NORMAL=args.vcf_NORMAL,
+                        sample_id=sample_id,
+                        normal_id=normal_id,
+                        input_vcf=os.path.join(vep_dir, file),
+                        maf_center=args.maf_center,
+                        maf=maf_file)
+                cmd = s.substitute(d)
+                print (cmd)
+                out_file = os.path.join(tmp_dir,"maf_script","{}.sh".format(file.split(".")[0]))
+                with open(out_file, 'w') as of:
+                    of.write("#!/bin/bash\n")
+                    of.write("#$ -cwd\n")
+                    of.write("#$ -S /bin/bash\n")
+                    of.write("module load perl/5.18.1\n")
+                    of.write("module load vep/83\n")
+                    of.write("module load samtools/1.3.1\n")
+                    of.write(cmd)
+                print (out_file)
+                os.system("chmod 755 %s"%out_file)
+
+if __name__ == "__main__":
+    main()
